330a5713142fa1cac075a6b14b876e30964f9af1
caio <caiothalles607@gmail.com>
2024-06-27 14:07:17 -0300

    feat: connection for MongoDb

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://mongo:12345@cluster0.ybcmfpp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
+
+app.use(express.json());
+app.use(morgan("dev"));
 
 app.listen(3333, () => console.log("server is run"));
